779ee0e31cfd40224d6bc2ecf1456592e91e5857
Daniel Carvalho <dco3@cin.ufpe.br>
2021-11-20 01:05:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+// import { db } from './services/firebase';
+import { useEffect } from "react";
+import { db } from "./services/firebase";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // caminho da collection
+  const usersCollectionRef = collection(db, "acidentes");
+
+  
+
+  
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      const dados = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      console.log(dados);
+    };
+
+    getUsers();
+  }, []);
+
+  return <div className="App">Request</div>;
 }
 
 export default App;
